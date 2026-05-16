c4fb9684d7c224517d9d165d092dd45dce26947b
Coolchickenguy <slightlylargersoft@protonmail.com>
2026-05-16 10:33:14 -0600

    Delete loader.js

--- simpleload/loader.js
+++
@@ -1,5 +0,0 @@
-import { register } from "node:module";
-import { pathToFileURL } from "node:url";
-
-// Register your custom loader
-register(new URL("./resolve.js", import.meta.url), pathToFileURL("./"));
